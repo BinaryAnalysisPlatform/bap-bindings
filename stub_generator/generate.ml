06f1159abbbc2ce0dedfa42dc90f526ef003bb3a
Ivan Gotovchits <ivg@ieee.org>
2017-01-09 15:41:25 -0500

    exposed bitvector interface

--- stub_generator/generate.ml
+++ stub_generator/generate.ml
@@ -1,3 +1,5 @@
+open Format
+
 let generate dirname =
   let prefix = "bap" in
   let path basename = Filename.concat dirname basename in
@@ -6,18 +8,16 @@ let generate dirname =
   let h_fd = open_out (path "bap.h") in
   let stubs = (module Bindings.Make : Cstubs_inverted.BINDINGS) in
   begin
-    (* Generate the ML module that links in the generated C. *)
     Cstubs_inverted.write_ml
-      (Format.formatter_of_out_channel ml_fd) ~prefix stubs;
+      (formatter_of_out_channel ml_fd) ~prefix stubs;
 
-    (* Generate the C source file that exports OCaml functions. *)
-    Format.fprintf (Format.formatter_of_out_channel c_fd)
+    fprintf (formatter_of_out_channel c_fd)
       "#include \"bap.h\"@\n%a"
       (Cstubs_inverted.write_c ~prefix) stubs;
 
-    (* Generate the C header file that exports OCaml functions. *)
-    Cstubs_inverted.write_c_header
-      (Format.formatter_of_out_channel h_fd) ~prefix stubs;
+    fprintf (formatter_of_out_channel h_fd)
+      "#include <stdint.h>@\n%a"
+    (Cstubs_inverted.write_c_header ~prefix) stubs;
 
   end;
   close_out h_fd;
