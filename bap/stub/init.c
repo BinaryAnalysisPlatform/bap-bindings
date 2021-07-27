0682c936f595d19c936a9c9c08c4a96ade2cdb93
ivg <ivg@ieee.org>
2021-07-27 17:00:46 -0400

    adds a proper error handling to `bap_init`

--- bap/stub/init.c
+++ bap/stub/init.c
@@ -5,6 +5,7 @@ void bap__stdio_init(FILE *, FILE *, FILE *);
 
 struct bap_parameters_t;
 int bap__main_init(struct bap_parameters_t *);
+const char * bap_error_get(void);
 
 int bap_init2(int argc, const char **argv, struct bap_parameters_t *pars) {
     caml_startup((char **)argv);
@@ -13,5 +14,8 @@ int bap_init2(int argc, const char **argv, struct bap_parameters_t *pars) {
 }
 
 void bap_init(int argc, const char **argv) {
-    bap_init2(argc,argv,NULL);
+    if (bap_init2(argc,argv,NULL)) {
+        fprintf(stderr, "failed to initialize BAP: %s\n", bap_error_get());
+        exit(1);
+    }
 }
